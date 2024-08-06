7584a29d4113f13c0f0fcb3039eee53dc19d855e
b_e_sa <joaospereira963@gmail.com>
2024-08-06 14:19:54 -0300

    init

---
+++ prisma/migrations/20240806165717_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT,
+    "description" TEXT,
+    "color" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "favorite" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER,
+    CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
---
+++ src/controllers/todo/create-todo.controller.ts
@@ -0,0 +1,50 @@
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
+import { CurrentUser } from '@/auth/current-user.decorator';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { TokenPayload } from '@/auth/jwt.strategy';
+import { ZodValidationPipe } from '@/pipes/zod-validation.pipe';
+import { PrismaService } from '@/prisma/prisma.service';
+import { z } from 'zod';
+
+const createTodoBodySchema = z.object({
+  content: z.string(),
+  title: z.string(),
+});
+
+type CreateTodoBodySchema = z.infer<typeof createTodoBodySchema>;
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateTodoController {
+  constructor(private prisma: PrismaService) {}
+
+  private convertToSlug(title: string) {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u336f]/q, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
+  }
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(
+    @Body() body: CreateQuestionBodySchema,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const { title, content } = body;
+    const userId = user.sub;
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug: 'asd',
+      },
+    });
+
+    return 'ok';
+  }
+}
